8016ab245c261d1fda02e39346679b6cd90afec5
TabithaKoech <tabbyk1718@gmail.com>
2022-08-21 21:20:38 +0300

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate", 1, "M", "red", "sad",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy", 1, "M", "red", "sad",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha", 1, "F", "red", "sad",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown", 1, "M", "red", "sad",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mellissa", 1, "F", "red", "sad",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch", 1, "F", "red", "sad",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy", 1, "F", "red", "sad",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, 1, "F", "red", "sad",1);
+
+
+
+
+
+
+
+
+
+
